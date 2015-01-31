4aff0d2d26827abaf5a21eee7150e8b5592f2cf3
Dzenan Ridjanovic <dzenanr@gmail.com>
2015-01-30 19:48:28 -0500

    drop toString

--- lib/fizz_buzz.dart
+++ lib/fizz_buzz.dart
@@ -99,7 +99,7 @@ List<int> fizzBuzz08([int count = 100]) {
   return new Iterable.generate(count, (i) => i + 1).map((i) => 
       '${(i % 3 == 0 ? "Fizz" : "")}'
       '${(i % 5 == 0 ? "Buzz" : "")}'
-      '${(i % 3 != 0 && i % 5 != 0 ? i.toString() : "")}').toList(); 
+      '${(i % 3 != 0 && i % 5 != 0 ? i : "")}').toList(); 
 }
 
 
